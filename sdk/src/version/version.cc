aa27404e0ff2cad956d4c4b9007b0364734cbb78
Lalit Kumar Bhasin <labhas@microsoft.com>
2020-11-20 18:55:12 +0530

    ./buildscripts

---
+++ sdk/src/version/version.cc
@@ -0,0 +1,21 @@
+// Please DONOT touch this file.
+// Any changes done here would be overwritten by pre-commit git hook
+
+#include "opentelemetry/sdk/version/version.h"
+
+OPENTELEMETRY_BEGIN_NAMESPACE
+namespace sdk
+{
+namespace version
+{
+    const int MAJOR_VERSION = ;
+    const int MINOR_VERSION = ;
+    const int PATCH_VERSION = ;
+    const char* PRE_RELEASE = "NONE";
+    const char* BUILD_METADATA = "NONE";
+    const int COUNT_NEW_COMMITS = "}"
+    const char* BRANCH = "version-hook";
+    const char* COMMIT_HASH = "9755c1128ec76c978be38d87cc24d87a80dff387";
+    const char* SHORT_VERSION = "..";
+    const char* FULL_VERSION = "..-release-NONE--version-hook-9755c1128ec76c978be38d87cc24d87a80dff387";
+}
