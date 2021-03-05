34755c968d9a3061522bfed8e31ed28de82dae8b
Lalit Kumar Bhasin <labhas@microsoft.com>
2021-03-06 00:53:41 +0530

    add grpc cache

--- ci/setup_grpc.sh
+++ ci/setup_grpc.sh
@@ -2,24 +2,47 @@
 
 set -e
 export DEBIAN_FRONTEND=noninteractive
+setup_type="build_and_install"
+if [ -z $1 ]; then
+  case "$1" in
+     build)
+        setup_type="build"
+        ;;
+     install)
+        setup_type="install"
+        ;;
+     *)
+        setup_type="build_and_install"
+        ;;
+  esac
+fi
 
 if ! type cmake > /dev/null; then
     #cmake not installed, exiting
     exit 1
 fi
-export BUILD_DIR=/tmp/
+export BUILD_DIR=/tmp/.grpc_build
 export INSTALL_DIR=/usr/local/
-pushd $BUILD_DIR
-git clone --recurse-submodules -b v1.34.0 https://github.com/grpc/grpc
-cd grpc
-mkdir -p cmake/build
-pushd cmake/build
-cmake -DgRPC_INSTALL=ON \
-    -DgRPC_BUILD_TESTS=OFF \
-    -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
-    ../..
-make -j $(nproc)
-make install
-popd
-popd
+if [ $setup_type == "build" ] || [ $setup_type == "build_and_install" ];  then
+    mkdir -p  ${BUILD_DIR}
+    rm -rf ${BUILD_DIR}/*
+    pushd $BUILD_DIR
+    git clone --recurse-submodules -b v1.34.0 https://github.com/grpc/grpc
+    cd grpc
+    mkdir -p cmake/build
+    pushd cmake/build
+    cmake -DgRPC_INSTALL=ON \
+        -DgRPC_BUILD_TESTS=OFF \
+        -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+        ../..
+    make -j $(nproc)
+    make install
+    popd
+    popd
+fi
+if [ $setup_type == "install" ] || [ $setup_type == "build_and_install" ];  then
+    pushd $BUILD_DIR
+    make install
+    popd
+fi
 export PATH=${INSTALL_DIR}/bin:$PATH  # ensure to use the installed grpc
